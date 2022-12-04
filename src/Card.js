5f50d89df11b88466f207f043997593c4688106c
AmarKaur08 <101163444+AmarKaur08@users.noreply.github.com>
2022-12-04 13:34:13 +0530

    1 file modified

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card=(props)=>
   
   
     return(
-      <div class="bgd">
+      <div className="bgd">
         <img src="https://robohash.org/ROBO1" alt="ROBO"/>
         <h1>{props.name}</h1>
         <p>{props.email}</p>        
