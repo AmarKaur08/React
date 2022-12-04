081e455f77386438af48b7e96ec6243e3eda17cb
AmarKaur08 <101163444+AmarKaur08@users.noreply.github.com>
2022-12-04 13:36:34 +0530

    1 file modified

--- src/Card.js
+++ src/Card.js
@@ -6,7 +6,7 @@ const Card=(props)=>
   
     return(
       <div className="bgd">
-        <img src="https://robohash.org/ROBO1" alt="ROBO"/>
+        <img src={`https://robohash.org/${props.name}`} alt="ROBO"/>
         <h1>{props.name}</h1>
         <p>{props.email}</p>        
           </div>
